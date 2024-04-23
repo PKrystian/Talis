c3f427a018de02ff3ba451c65cb0e6d80b2e3814
PKrystian <krystian.pinczak2001@gmail.com>
2024-04-23 21:08:44 +0200

    hello-world aws

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, World!
+        </p>
+        <p>
+            Talis test app.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
