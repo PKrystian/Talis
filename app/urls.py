bd5926bfe4927e09c929a952d5a2461c23e02edc
PKrystian <krystian.pinczak2001@gmail.com>
2024-05-12 20:49:05 +0200

    Project setup auto build frontend

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,5 @@
-from django.urls import path
 from . import views
-from django.conf import settings
-from django.urls import path, include
-from django.conf.urls.static import static
-
+from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
