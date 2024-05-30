36ce092c4ca1d8cd8e87d45730f2f5dc7532a605
Mikolaj Bigaj <mikolaj.bigaj.00@gmail.com>
2024-05-31 01:07:52 +0200

    CU-8694m68b3 fix 	Fixed migration not working 	Fixed staticmethod not being able to be used

--- app/models/board_game.py
+++ app/models/board_game.py
@@ -1,31 +1,32 @@
 from django.db import models
 
 
+def default_category() -> dict[str, list]:
+    return {'category': []}
+
+
+def default_mechanic() -> dict[str, list]:
+    return {'mechanic': []}
+
+
+def default_expansion() -> dict[str, list]:
+    return {'expansion': []}
+
+
 class BoardGame(models.Model):
-    @staticmethod
-    def __default_category() -> dict[str, list]:
-        return {'category': []}
-
-    @staticmethod
-    def __default_mechanic() -> dict[str, list]:
-        return {'mechanic': []}
-
-    @staticmethod
-    def __default_expansion() -> dict[str, list]:
-        return {'expansion': []}
 
     objects = None
     name = models.CharField(max_length=256, unique=True)
     year_published = models.PositiveSmallIntegerField(null=True)
     publisher = models.CharField(max_length=128, null=True)
-    category = models.JSONField(default=__default_category)
-    mechanic = models.JSONField(default=__default_mechanic)
+    category = models.JSONField(default=default_category)
+    mechanic = models.JSONField(default=default_mechanic)
     min_players = models.PositiveSmallIntegerField(default=0, null=True)
     max_players = models.PositiveSmallIntegerField(default=0, null=True)
     age = models.PositiveSmallIntegerField(default=0, null=True)
     min_playtime = models.PositiveIntegerField(default=0, null=True)
     max_playtime = models.PositiveIntegerField(default=0, null=True)
-    expansion = models.JSONField(default=__default_expansion)
+    expansion = models.JSONField(default=default_expansion)
     description = models.TextField(default='', null=True)
     image_url = models.CharField(max_length=256, unique=True, null=True)
 
