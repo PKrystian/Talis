e3acd21e4ea3e6595a90eb8aa590c55f0edf7a22
PKrystian <krystian.pinczak2001@gmail.com>
2024-06-07 00:10:23 +0200

    CU-8694m660t - Create a Basic Table like structure with empty boxes for Board Game images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
-import './App.css';
-import React from 'react';
-import BoardGameList from './components/BoardGameList';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import LandingPage from './components/LandingPage';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStarOfLife, faListCheck, faChartLine, faUsers, faHandsClapping } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStarOfLife, faListCheck, faChartLine, faUsers, faHandsClapping);
 
 const App = () => {
-return (
-    <div className="App">
-        <header className="App-header">
-            <BoardGameList/>
-        </header>
+  const [boardGames, setBoardGames] = useState({});
+
+  useEffect(() => {
+    const apiUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/board-games/' : '/api/board-games/';
+
+    const fetchBoardGames = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        setBoardGames(response.data);
+      } catch (error) {
+        console.error('Error fetching boardGames:', error);
+      }
+    };
+
+    fetchBoardGames().then();
+  }, []);
+
+  return (
+    <div>
+      <LandingPage boardGames={boardGames} />
     </div>
-);}
+  );
+}
 
 export default App;
