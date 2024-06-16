c8d652472ac595d3fcdc53525b2f37ff36b83dac
PKrystian <krystian.pinczak2001@gmail.com>
2024-06-16 20:01:09 +0200

    fix

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,9 @@
 from . import views
-from django.urls import path, re_path
-from django.views.generic import TemplateView
+from django.urls import path
 
 API_PREFIX = 'api/'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path(API_PREFIX + 'board-games/', views.board_game_list, name='board-game-list'),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.views.static import serve
 
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^contact/', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^about/', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^user/', TemplateView.as_view(template_name='index.html')),
 ]
