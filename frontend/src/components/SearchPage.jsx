76664ee0d92250ea6a05a29e8213cd1b109b4173
PMusielak <musielak.pawel02@gmail.com>
2024-07-12 13:30:11 +0200

    U-8694v8khe_Prepare-a-board-game-search-view-for-entered-search-string_Pawe-Musielak feature

--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -3,19 +3,27 @@ import TableItem from './TableItem';
 import './SearchPage.css';
 
 const SearchPage = ({boardGames}) => {
+    const recommended_placeholder = "Wishlist";
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const query = params.get('query');
+    console.log(boardGames);
     var sgame = boardGames.Wishlist.filter(game => game.name.toLowerCase().includes(query.toLowerCase()));
     return (
       <div>
-        <div className="container text-center">
-          <h1>Search Page for: {query}</h1>
-            <div className="d-flex">
-                {sgame.map(game => (
-                    <TableItem key={game.id} boardGame={game} />
-                    ))}
-            </div>
+        <div className="container text-left">
+          <h1>Based on the search phrase: "{query}"</h1>
+          <div className="d-flex search-results">
+              {sgame.map(game => (
+                  <TableItem key={game.id} boardGame={game} />
+                  ))}
+          </div>
+          <h1>Related to the search phrase</h1>
+          <div className="d-flex search-results">
+            {boardGames[recommended_placeholder].map(boardGame => (
+              <TableItem key={boardGame.id} boardGame={boardGame} />
+            ))}
+          </div>
         </div>
       </div>
     );
