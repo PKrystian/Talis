94a961293fddaa9aa33af2465985fd71028fe5c5
PKrystian <krystian.pinczak2001@gmail.com>
2024-07-24 21:42:08 +0200

    final feature implemented, final filters

--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -20,21 +20,31 @@ const SearchPage = () => {
   const [filters, setFilters] = useState({
     category: filterType === 'Category' ? filter.split(',') : [],
     mechanic: filterType === 'Mechanic' ? filter.split(',') : [],
-    publisher: ''
+    minPlayers: '',
+    maxPlayers: '',
+    age: [],
+    playtime: []
   });
   const [expandedFilter, setExpandedFilter] = useState({
     category: false,
-    mechanic: false
+    mechanic: false,
+    players: false,
+    age: false,
+    playtime: false
   });
 
   const handleInputChange = (e) => {
     const { name, value, checked } = e.target;
     setFilters((prevFilters) => {
-      const updatedFilters = checked
-        ? [...prevFilters[name], value]
-        : prevFilters[name].filter((item) => item !== value);
+      if (name === 'minPlayers' || name === 'maxPlayers') {
+        return { ...prevFilters, [name]: value };
+      } else {
+        const updatedFilters = checked
+          ? [...prevFilters[name], value]
+          : prevFilters[name].filter((item) => item !== value);
 
-      return { ...prevFilters, [name]: updatedFilters };
+        return { ...prevFilters, [name]: updatedFilters };
+      }
     });
   };
 
@@ -48,42 +58,68 @@ const SearchPage = () => {
   const applyFilters = () => {
     const searchParams = new URLSearchParams();
     searchParams.append('query', query);
-    if (filters.category.length > 0) {
-      searchParams.append('filterType', 'Category');
-      searchParams.append('filter', filters.category.join(','));
+
+    ['category', 'mechanic', 'age', 'playtime'].forEach(filterType => {
+      filters[filterType].forEach(filterValue => {
+        searchParams.append('filters', `${filterType}|${filterValue}`);
+      });
+    });
+
+    if (filters.minPlayers || filters.maxPlayers) {
+      searchParams.append('filters', `players|${filters.minPlayers}-${filters.maxPlayers}`);
     }
-    if (filters.mechanic.length > 0) {
-      searchParams.append('filterType', 'Mechanic');
-      searchParams.append('filter', filters.mechanic.join(','));
-    }
+
     navigate(`?${searchParams.toString()}`);
   };
 
   useEffect(() => {
-    if (query || filter) {
-      setIsLoading(true);
-      axios.get('/api/search/', { params: { query, limit: 48, filterType, filter } })
-        .then(response => {
-          setSearchResults(response.data.results);
-          setIsLoading(false);
-        })
-        .catch(error => {
-          setError(error);
-          setIsLoading(false);
-        });
-    } else {
-      setSearchResults([]);
-      setIsLoading(false);
-    }
-  }, [query, filterType, filter]);
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get('query') || '';
+    const filters = searchParams.getAll('filters');
+
+    setIsLoading(true);
+    axios.get('/api/search/', {
+      params: {
+        query,
+        limit: 48,
+        filters // Pass the filters directly as they are now in the correct format
+      }
+    })
+      .then(response => {
+        setSearchResults(response.data.results);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [location.search]); // Listen for changes in the search part of the URL
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    setFilters({
-      category: searchParams.get('filterType') === 'Category' ? searchParams.get('filter').split(',') : [],
-      mechanic: searchParams.get('filterType') === 'Mechanic' ? searchParams.get('filter').split(',') : [],
-      publisher: ''
+    const newFilters = {
+      category: [],
+      mechanic: [],
+      minPlayers: '',
+      maxPlayers: '',
+      age: [],
+      playtime: []
+    };
+
+    searchParams.getAll('filters').forEach(filter => {
+      const [filterType, filterValue] = filter.split('|');
+      if (filterType && filterValue) {
+        if (filterType === 'category' || filterType === 'mechanic' || filterType === 'age' || filterType === 'playtime') {
+          newFilters[filterType].push(filterValue);
+        } else if (filterType === 'players') {
+          const [minPlayers, maxPlayers] = filterValue.split('-');
+          newFilters.minPlayers = minPlayers;
+          newFilters.maxPlayers = maxPlayers;
+        }
+      }
     });
+
+    setFilters(newFilters);
   }, [location.search]);
 
   if (isLoading) {
@@ -151,6 +187,85 @@ const SearchPage = () => {
               </div>
             )}
           </div>
+          <div className="filter-group">
+            <div className="filter-header" onClick={() => toggleFilterSection('players')}>
+              <h5>Number of Players {expandedFilter.players ? <FaChevronUp /> : <FaChevronDown />}</h5>
+            </div>
+            {expandedFilter.players && (
+              <div className="filter-options">
+                <div className="d-flex">
+                  <input
+                    type="number"
+                    id="minPlayers"
+                    name="minPlayers"
+                    value={filters.minPlayers}
+                    onChange={handleInputChange}
+                    placeholder="Min"
+                    className="form-control me-2"
+                  />
+                  <input
+                    type="number"
+                    id="maxPlayers"
+                    name="maxPlayers"
+                    value={filters.maxPlayers}
+                    onChange={handleInputChange}
+                    placeholder="Max"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="filter-group">
+            <div className="filter-header" onClick={() => toggleFilterSection('age')}>
+              <h5>Age {expandedFilter.age ? <FaChevronUp /> : <FaChevronDown />}</h5>
+            </div>
+            {expandedFilter.age && (
+              <div className="filter-options">
+                {['up to 3 years', '3-4 years', '5-7 years', '8-11 years', '12-14 years', '15-17 years', '18+ years'].map(age => (
+                  <div key={age} className="form-check">
+                    <input
+                      type="checkbox"
+                      id={`age-${age}`}
+                      name="age"
+                      value={age}
+                      checked={filters.age.includes(age)}
+                      onChange={handleInputChange}
+                      className="form-check-input"
+                    />
+                    <label htmlFor={`age-${age}`} className="form-check-label text-light">
+                      {age}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="filter-group">
+            <div className="filter-header" onClick={() => toggleFilterSection('playtime')}>
+              <h5>Playtime {expandedFilter.playtime ? <FaChevronUp /> : <FaChevronDown />}</h5>
+            </div>
+            {expandedFilter.playtime && (
+              <div className="filter-options">
+                {['< 15 min', '< 30 min', '< 1h', '< 2h', '2h+'].map(playtime => (
+                  <div key={playtime} className="form-check">
+                    <input
+                      type="checkbox"
+                      id={`playtime-${playtime}`}
+                      name="playtime"
+                      value={playtime}
+                      checked={filters.playtime.includes(playtime)}
+                      onChange={handleInputChange}
+                      className="form-check-input"
+                    />
+                    <label htmlFor={`playtime-${playtime}`} className="form-check-label text-light">
+                      {playtime}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
         <div className="col-md-10">
           <div className="d-flex flex-wrap">
@@ -159,7 +274,7 @@ const SearchPage = () => {
                 <TableItem key={boardGame.id} boardGame={boardGame} />
               ))
             ) : (
-              ""
+              <div>No results found.</div>
             )}
           </div>
         </div>
