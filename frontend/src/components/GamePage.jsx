5802fda0166a8d1532d055551547538495bebbc3
PMusielak <musielak.pawel02@gmail.com>
2024-07-31 18:34:38 +0200

    CU-869500fz7 feature

--- frontend/src/components/GamePage.jsx
+++ frontend/src/components/GamePage.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers, faClock, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faUsers, faClock, faStar, faClipboardList, faPlus, faShare } from '@fortawesome/free-solid-svg-icons';
 import './GamePage.css';
 
 const GamePage = () => {
@@ -10,7 +10,7 @@ const GamePage = () => {
     const boardGame = boardGameString ? JSON.parse(decodeURIComponent(boardGameString)) : null;
     return (
     <div className="container">
-        <div className="game-content">
+        <div className="game-page-basic-info">
             <div className="game-image">
                 <img src={ boardGame.image_url } className="boardgame-img" alt={ boardGame.name }/>
             </div>
@@ -37,7 +37,38 @@ const GamePage = () => {
                 <div className="additional-info-item">
                     <p><FontAwesomeIcon icon={faStar} className="nav-icon basic-game-icon" /><span style={{ marginLeft: '5px', verticalAlign: '12px' }}>7.7/10</span></p>
                 </div>
+                <div className="game-page-user-action">
+                    <div className="game-page-user-action-item">
+                        <p><FontAwesomeIcon icon={faClipboardList} className="nav-icon basic-game-icon" /></p>
+                        <p>Add to Wishlist</p>
+                    </div>
+                    <div className="game-page-user-action-item">
+                        <p><FontAwesomeIcon icon={faPlus} className="nav-icon basic-game-icon" /></p>
+                        <p>Add to Library</p>
+                    </div>
+                    <div className="game-page-user-action-item">
+                        <p><FontAwesomeIcon icon={faShare} className="nav-icon basic-game-icon" /></p>
+                        <p>Share</p>
+                    </div>
+                </div>
+                <div className="game-page-friends-info">
+                    <p>Friends that already have this game:</p>
+                    <div className="game-page-friend-icons">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                    <p>Friends that wishlisted this game:</p>
+                    <div className="game-page-friend-icons">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div className="description">
+            <h1>Description:</h1>
+            <p>{boardGame.description}</p>
         </div>
     </div>
     );
