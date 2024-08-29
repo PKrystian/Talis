ce0140a6baabde027d5226cec2a744c2fb9c5f8c
PKrystian <krystian.pinczak2001@gmail.com>
2024-08-29 20:44:58 +0200

    CU-8695jm36y_URL-Cleanup-and-Frontend-Enhancements_Krystian-Piczak feature

--- app/controllers/BoardGameController.py
+++ app/controllers/BoardGameController.py
@@ -1,4 +1,5 @@
 import random
+from django.db.models import Q
 from django.http import JsonResponse
 
 from app.controllers.SearchController import SearchController
@@ -9,11 +10,17 @@ class BoardGameController:
     ROUTE: str = 'board-games/'
 
     def action_board_game_list(self) -> JsonResponse:
-        board_games = BoardGame.objects.exclude(rating__isnull=True).prefetch_related(
+        board_games = BoardGame.objects.filter(
+            Q(rating__isnull=False) &
+            Q(year_published__isnull=False)
+        ).prefetch_related(
             'boardgamepublisher_set__publisher',
             'boardgamecategory_set__category',
             'expansions__expansion_board_game'
-        ).order_by('-rating')[:SearchController.MEDIUM_LIMIT]
+        ).filter(
+            Q(boardgamepublisher__isnull=False) &
+            Q(boardgamecategory__isnull=False)
+        ).distinct().order_by('-rating')[:SearchController.MEDIUM_LIMIT]
 
         data = []
 
@@ -56,6 +63,44 @@ class BoardGameController:
 
         categorized_games = {category: self.get_shuffled_games(board_games) for category in categories}
         return categorized_games
+
+    ROUTE_GAME_DETAIL: str = 'board-games/<int:game_id>/'
+
+    @staticmethod
+    def action_board_game_detail(request, game_id) -> JsonResponse:
+        try:
+            board_game = BoardGame.objects.prefetch_related(
+                'boardgamepublisher_set__publisher',
+                'boardgamecategory_set__category',
+                'expansions__expansion_board_game'
+            ).get(id=game_id)
+
+            publishers = ', '.join([bp.publisher.name for bp in board_game.boardgamepublisher_set.all()])
+            categories = ', '.join([bc.category.name for bc in board_game.boardgamecategory_set.all()])
+            expansions = [{
+                'expansion_id': expansion.expansion_board_game.id,
+                'expansion_name': expansion.expansion_board_game.name
+            } for expansion in board_game.expansions.all()]
+
+            data = {
+                'id': board_game.id,
+                'name': board_game.name,
+                'year_published': board_game.year_published,
+                'publisher': publishers,
+                'category': categories,
+                'expansions': expansions,
+                'description': board_game.description,
+                'min_players': board_game.min_players,
+                'max_players': board_game.max_players,
+                'age': board_game.age,
+                'min_playtime': board_game.min_playtime,
+                'max_playtime': board_game.max_playtime,
+                'image_url': board_game.image_url,
+            }
+
+            return JsonResponse(data)
+        except BoardGame.DoesNotExist:
+            return JsonResponse({'error': 'BoardGame not found'}, status=404)
 
     @staticmethod
     def get_shuffled_games(board_games) -> list:
--- frontend/src/components/TableItem.jsx
+++ frontend/src/components/TableItem.jsx
@@ -5,44 +5,21 @@ const TableItem = ({ boardGame }) => {
   const navigate = useNavigate();
 
   const onClick = (boardGame) => {
-    const boardGameString = encodeURIComponent(JSON.stringify(boardGame));
-    navigate(`/game?boardGame=${ boardGameString }`);
+    navigate(`/game/${boardGame.id}`);
   };
-
-  const renderRangeText = (label, value1, value2 = null, suffix = '') => {
-    if (! value1 && ! value2) return null;
-    const text = value1 === value2 ? value1 : `${ value1 } - ${ value2 }`;
-    return (
-      <p className="card-text">
-        <span className="fw-bold">{ label }: </span> { text } { suffix }
-      </p>
-    );
-  };
-
-  const renderText = (label, value) => (
-    value && <p className="card-text"><span className="fw-bold">{ label }: </span> { value }</p>
-  );
 
   return (
-    <div key={ boardGame.id } className="card bg-dark text-white m-2">
-      <img
-        src={ boardGame.image_url }
-        className="card-img-top"
-        alt={ boardGame.name }
-        onClick={ () => onClick(boardGame) }
-      />
-      <div className="card-body">
-        <h5 className="card-title" onClick={ () => onClick(boardGame) }>{ boardGame.name }</h5>
-        { renderText("Producer", boardGame.publisher) }
-        { renderText("Categories", boardGame.category) }
-        { renderText("Year", boardGame.year_published ) }
-        { renderRangeText("Players", boardGame.min_players, boardGame.max_players) }
-        { renderRangeText("Playtime", boardGame.min_playtime, boardGame.max_playtime, "mins") }
-        { boardGame.age !== undefined && (
-          <p className="card-text">
-            <span className="fw-bold">Age: </span> { boardGame.age === 0 ? "Everyone" : `${ boardGame.age }+` }
-          </p>
-        )}
+    <div key={boardGame.id} className="card bg-dark text-white m-2">
+      <div className="card-img-wrapper">
+        <img
+          src={boardGame.image_url}
+          className="card-img-top"
+          alt={boardGame.name}
+          onClick={() => onClick(boardGame)}
+        />
+        <h5 className="card-title" onClick={() => onClick(boardGame)}>
+          {boardGame.name}
+        </h5>
       </div>
     </div>
   );
