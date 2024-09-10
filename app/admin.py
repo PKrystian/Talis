1008300b14960480a8550658c83b742d83c092e0
MBigaj <mikolaj.bigaj.00@gmail.com>
2024-09-10 20:33:48 +0200

    CU-8695qrn6v feature
    Added Logger and LoggerCreator for 3 different danger levels

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models.board_game import BoardGame
+from .models.log_error import LogError
 from .models.registered_user import RegisteredUser
 
 admin.site.register(BoardGame)
 admin.site.register(RegisteredUser)
+admin.site.register(LogError)
