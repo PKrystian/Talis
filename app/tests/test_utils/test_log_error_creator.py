b4afc5d0461920be36f5f04efe4ced2e3d93395c
MBigaj <mikolaj.bigaj.00@gmail.com>
2024-10-10 20:15:07 +0200

    CU-8696592fp feature
    Added test for LogErrorCreator and all of the danger levels

---
+++ app/tests/test_utils/test_log_error_creator.py
@@ -0,0 +1,65 @@
+import pytest
+
+from app.models.log_error import LogError
+from app.utils.LogErrorCreator import LogErrorCreator
+
+
+class TestLogErrorCreator:
+    log_error_creator: LogErrorCreator
+
+    def setup_class(self):
+        self.log_error_creator = LogErrorCreator()
+
+    @pytest.mark.django_db
+    def test_log_debug(self):
+        message = 'Test Debug Log'
+        trigger = 'TestCaseDebug'
+
+        (
+            self.log_error_creator
+            .create()
+            .debug()
+            .log(message, trigger, str(self.__class__))
+        )
+
+        saved_log = LogError.objects.filter(trigger__exact=trigger).get()
+
+        self.__assert(saved_log, message, LogError.DEBUG)
+
+    @pytest.mark.django_db
+    def test_log_warning(self):
+        message = 'Test Warning Log'
+        trigger = 'TestCaseWarning'
+
+        (
+            self.log_error_creator
+            .create()
+            .warning()
+            .log(message, trigger, str(self.__class__))
+        )
+
+        saved_log = LogError.objects.filter(trigger__exact=trigger).get()
+
+        self.__assert(saved_log, message, LogError.WARNING)
+
+    @pytest.mark.django_db
+    def test_log_critical(self):
+        message = 'Test Critical Log'
+        trigger = 'TestCaseCritical'
+
+        (
+            self.log_error_creator
+            .create()
+            .critical()
+            .log(message, trigger, str(self.__class__))
+        )
+
+        saved_log = LogError.objects.filter(trigger__exact=trigger).get()
+
+        self.__assert(saved_log, message, LogError.CRITICAL)
+
+    @staticmethod
+    def __assert(saved_log, message, danger_level):
+        assert(type(saved_log) == LogError)
+        assert(saved_log.message == message)
+        assert(saved_log.danger_level == danger_level)
