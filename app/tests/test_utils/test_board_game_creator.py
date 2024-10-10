ee0e3647a1047c71fa8801db1bcf96c8444eee78
MBigaj <mikolaj.bigaj.00@gmail.com>
2024-10-10 15:38:05 +0200

    CU-86964prcw feature
    Added test for loading from dataframe
    Updated Creator with self return

--- app/tests/test_utils/test_board_game_creator.py
+++ app/tests/test_utils/test_board_game_creator.py
@@ -1,36 +1,63 @@
+import pandas as pd
 import pytest
 
 from app.models.board_game import BoardGame
 from app.utils.BoardGameCreator import BoardGameCreator
-from app.utils.bgg_api import api_params
+from app.utils.bgg_api import bgg_api_params
+from app.views import index
 
 
 class TestBoardGameCreator:
+    board_game_creator: BoardGameCreator
+
+    def setup_class(self):
+        self.board_game_creator = BoardGameCreator()
+
     @pytest.fixture
     def board_game_data_dict_fixture(self):
-        return {
-            api_params.NAME: 'Test Board Game',
-            api_params.YEAR_PUBLISHED: 2000,
-            api_params.MIN_PLAYERS: 3,
-            api_params.MAX_PLAYERS: 12,
-            api_params.AGE: 12,
-            api_params.MIN_PLAYTIME: 60,
-            api_params.MAX_PLAYTIME: 120,
-            api_params.DESCRIPTION: 'This game really slaps',
+        yield {
+            bgg_api_params.NAME: 'Test Board Game',
+            bgg_api_params.YEAR_PUBLISHED: 2000,
+            bgg_api_params.MIN_PLAYERS: 3,
+            bgg_api_params.MAX_PLAYERS: 12,
+            bgg_api_params.AGE: 12,
+            bgg_api_params.MIN_PLAYTIME: 60,
+            bgg_api_params.MAX_PLAYTIME: 120,
+            bgg_api_params.DESCRIPTION: 'This game really slaps',
         }
-    
+
     def test_load_from_dict(self, board_game_data_dict_fixture):
-        board_game_creator = BoardGameCreator()
-        board_game_creator.create()
-        board_game_creator.load_from_dict(board_game_data_dict_fixture)
-        new_board_game = board_game_creator.get_board_game()
-        
-        assert(new_board_game.name == board_game_data_dict_fixture[api_params.NAME])
+        new_board_game = (
+            self.board_game_creator
+            .create()
+            .load_from_dict(board_game_data_dict_fixture)
+            .get_board_game()
+        )
+
+        assert(new_board_game.name == board_game_data_dict_fixture[bgg_api_params.NAME])
         assert(type(new_board_game) == BoardGame)
 
     @pytest.fixture
     def board_game_data_df_fixture(self):
-        return {}
+        yield pd.DataFrame(
+            data={
+                bgg_api_params.NAME: 'Totally new game never seen before',
+                bgg_api_params.YEAR_PUBLISHED: 2015,
+                bgg_api_params.MIN_PLAYERS: 10,
+                bgg_api_params.MAX_PLAYERS: 20,
+                bgg_api_params.AGE: 16,
+                bgg_api_params.DESCRIPTION: 'This game offers a wide variety of gameplay potential',
+            },
+            index=[0]
+        )
     
     def test_load_from_df(self, board_game_data_df_fixture):
-        assert('' == '')
+        new_board_game = (
+            self.board_game_creator
+            .create()
+            .load_from_dataframe(board_game_data_df_fixture)
+            .get_board_game()
+        )
+
+        assert((new_board_game.name == board_game_data_df_fixture[bgg_api_params.NAME]).any())
+        assert(type(new_board_game) == BoardGame)
