e6af03511bd6376f4cc9ef4f3c17a6ed0207206f
MBigaj <mikolaj.bigaj.00@gmail.com>
2024-10-13 17:38:56 +0200

    CU-86965vycw feature
    Created test for PhotonAPI

--- app/tests/test_utils/test_api_connections.py
+++ app/tests/test_utils/test_api_connections.py
@@ -4,3 +4,8 @@ import requests
 def test_bgg_api_connection():
         response = requests.get('https://api.geekdo.com/xmlapi/')
         assert(response.status_code == 200)
+
+
+def test_photon_api_connection():
+        response = requests.get('https://photon.komoot.io/api/?q')
+        assert(response.status_code == 200)
