9e821e72a27fa7afd94d1895874cc4a0d5414fc8
PKrystian <krystian.pinczak2001@gmail.com>
2024-10-17 16:21:31 +0200

    deploy fix 8 (fixed tests)

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
