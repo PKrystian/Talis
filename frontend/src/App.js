93e8a21a00abd98501466079d6a907b5a7fe41e2
PMusielak <musielak.pawel02@gmail.com>
2024-10-19 17:35:54 +0200

    added form for creating events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 import PolicyPage from './components/PolicyPage';
 import LicensePage from './components/LicensePage';
 import LoginModal from './components/utils/LoginModal';
+import CreateEventPage from './components/CreateEventPage';
 
 const App = () => {
   const apiPrefix =
@@ -107,6 +108,16 @@ const App = () => {
           />
           <Route path="/policy" element={<PolicyPage />} />
           <Route path="/license" element={<LicensePage />} />
+          <Route
+            path="/create-event"
+            element={
+              <CreateEventPage
+                apiPrefix={apiPrefix}
+                user={user}
+                userState={userState}
+              />
+            }
+          />
         </Routes>
         <Footer />
       </div>
