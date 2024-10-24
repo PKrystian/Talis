2b8ee555dffe08213b947233bf38d0205f708a3b
MBigaj <mikolaj.bigaj.00@gmail.com>
2024-10-24 12:25:33 +0200

    CU-8696aq4m6 feature
    Fixed dependencies for FriendList ( now properly creates for both sides
    when needed )

--- app/models/friend_list.py
+++ app/models/friend_list.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class FriendList(models.Model):
+    STATUS_PENDING = 'pending'
+    STATUS_ACCEPTED = 'accepted'
+    STATUS_REMOVED = 'removed'
+    STATUS_REJECTED = 'rejected'
+
     DoesNotExist = None
     objects = None
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_friend_list')
@@ -10,3 +15,33 @@ class FriendList(models.Model):
     status = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        super().save(force_insert, force_update, using, update_fields)
+
+        if self.status in [self.STATUS_PENDING, self.STATUS_REJECTED]:
+            return
+
+        if FriendList.objects.filter(
+            user_id__exact=self.friend.id,
+            friend_id__exact=self.user.id
+        ).exclude(status__exact=self.status).exists():
+            referenced_friend = FriendList.objects.filter(
+                user_id__exact=self.friend.id,
+                friend_id__exact=self.user.id
+            ).get()
+
+            referenced_friend.status = self.status
+            referenced_friend.save()
+        elif not FriendList.objects.filter(
+            user_id__exact=self.friend.id,
+            friend_id__exact=self.user.id,
+            status__exact=self.status
+        ).exists():
+            FriendList.objects.create(
+                user_id=self.friend.id,
+                friend_id=self.user.id,
+                status=self.status
+            )
